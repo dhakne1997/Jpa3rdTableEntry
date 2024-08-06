d3c462f25d974d7f0848a49a80ca90bdd1396111
dhakne1997 <neml10614@smbneml.in>
2024-08-06 10:59:39 +0530

    normal changes1

--- src/main/java/com/entity/Department.java
+++ src/main/java/com/entity/Department.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Data
+
 @Table(name="department_")
 public class Department {
 
@@ -20,6 +20,44 @@ public class Department {
     private String location;
     private String head;
     private int numOfFaculties;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDeptName() {
+		return deptName;
+	}
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getHead() {
+		return head;
+	}
+	public void setHead(String head) {
+		this.head = head;
+	}
+	public int getNumOfFaculties() {
+		return numOfFaculties;
+	}
+	public void setNumOfFaculties(int numOfFaculties) {
+		this.numOfFaculties = numOfFaculties;
+	}
+	public Department(Long id, String deptName, String location, String head, int numOfFaculties) {
+		super();
+		this.id = id;
+		this.deptName = deptName;
+		this.location = location;
+		this.head = head;
+		this.numOfFaculties = numOfFaculties;
+	}
 
 }
 
--- src/main/java/com/entity/Student.java
+++ src/main/java/com/entity/Student.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Data
+
 @Table(name="student_")
 public class Student {
 
@@ -19,6 +19,44 @@ public class Student {
     private int age;
     private String email;
     private String major;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	public Student(Long id, String name, int age, String email, String major) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.email = email;
+		this.major = major;
+	}
 
 
 }
